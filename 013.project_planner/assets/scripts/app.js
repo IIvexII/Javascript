ca3ff7c6e8b74345e00a00a973b57342508c4ccc
Zafeer Hafeez <mrh4ck3r1738@gmail.com>
2022-03-19 01:46:17 +0500

    All Buttons are working

---
+++ 013.project_planner/assets/scripts/app.js
@@ -0,0 +1,32 @@
+class App {
+  constructor () {
+    this.allProjects = document.querySelectorAll('li');
+  }
+  render() {
+    for (const project of this.allProjects) {
+      const infoBtn = project.querySelector('button:first-of-type');
+      const statusBtn = project.querySelector('button:last-of-type');
+      infoBtn.addEventListener('click', this.buttonEvent.bind(project, infoBtn));
+      statusBtn.addEventListener('click', this.buttonEvent.bind(project, statusBtn));
+    }
+  }
+  buttonEvent(btn) {
+    console.log(btn.textContent);
+    if (btn.textContent === 'Finish') {
+      const finishedProjects = document.querySelector('#finished-projects ul');  
+      btn.textContent = 'Activate';
+      finishedProjects.appendChild(this);
+    }
+    else if (btn.textContent === 'Activate') {
+      const finishedProjects = document.querySelector('#active-projects ul');  
+      btn.textContent = 'Finish';
+      finishedProjects.appendChild(this);
+    }
+    else if (btn.textContent === 'More Info') {
+      console.log('Info..');
+    }
+  }
+}
+
+const app = new App();
+app.render();
